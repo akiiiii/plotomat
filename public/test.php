ec1112ea8132bd97d4e9cd1846b14ab37e037df4
Sandy Lewanscheck <sandy@lewanscheck.de>
2012-08-08 13:25:11 +0200

    changed phpunit testing setup - working now

--- public/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-require_once '../tests/MyTest.php';
-require_once 'PHPUnit/Autoload.php';
-
-
-$suite  = new PHPUnit_Framework_TestSuite("MyTest");
-$suite->run();
---
+++ tests/application/bootstrap.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Just copied from /public/index.php and adjust directories
+ * AND assume testing as default environment instead of production
+ */
+error_reporting(E_ALL | E_STRICT);
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/*
+$incArray = array(	'/models/MongoDoc');
+
+foreach($incArray as $fileLocPath) {
+	$path = APPLICATION_PATH . $fileLocPath;
+	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+}
+*/
+
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+require_once 'Zend/Loader/Autoloader.php';
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->registerNamespace('Shanty_');
+$loader->registerNamespace('Lex0r_');
+
+require_once 'ControllerTestCase.php';
